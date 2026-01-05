25b9397394b48c5913c671ab4f5802aab859e92e
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2026-01-05 10:44:29 +0300

    feat: add initial Next.js configuration including TypeScript ignore, unoptimized images, and API rewrites

--- gmail-manager-frontend/next.config.mjs
+++ gmail-manager-frontend/next.config.mjs
@@ -7,10 +7,11 @@ const nextConfig = {
     unoptimized: true,
   },
   async rewrites() {
+    const destUrl = process.env.API_URL || 'http://127.0.0.1:8123';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8123/api/:path*',
+        destination: `${destUrl}/api/:path*`,
       },
     ]
   },

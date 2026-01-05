0c1ab45a2439f4ea847edad061cd1e8727d11f83
Rindra ANDRIANJANAKA <randrianjanaka@hellopro.fr>
2026-01-05 10:46:56 +0300

    feat: Add initial Next.js configuration to ignore TypeScript errors and disable image optimization.

--- gmail-manager-frontend/next.config.mjs
+++ gmail-manager-frontend/next.config.mjs
@@ -6,15 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    const destUrl = process.env.API_URL || 'http://127.0.0.1:8123';
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${destUrl}/api/:path*`,
-      },
-    ]
-  },
 
 }
 
